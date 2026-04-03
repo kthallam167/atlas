2c0e6d57f82630d0404d759813418a6e2edcac7f
Karthik Thallam <kthallam@andrew.cmu.edu>
2026-04-03 16:50:20 -0400

    Implement compressed postings block encoding with skip pointers

--- cpp/src/index_builder.cpp
+++ cpp/src/index_builder.cpp
@@ -1,5 +1,6 @@
 #include "atlas/index_builder.hpp"
 #include "atlas/tokenizer.hpp"
+#include "atlas/varbyte.hpp"
 #include <fstream>
 #include <stdexcept>
 #include <unordered_map>
@@ -13,6 +14,29 @@ struct TermPostings {
     std::vector<uint32_t> freqs;
     std::vector<uint32_t> positions;
 };
+
+constexpr uint32_t kSkipThreshold = 16;
+
+std::vector<uint8_t> encode_block(const TermPostings& tp) {
+    const uint32_t df = static_cast<uint32_t>(tp.docids.size());
+    std::vector<uint8_t> region;
+    uint32_t prev_doc = 0;
+    size_t pi = 0;
+
+    for (uint32_t i = 0; i < df; ++i) {
+        vbyte_encode(tp.docids[i] - prev_doc, region);
+        prev_doc = tp.docids[i];
+        const uint32_t tf = tp.freqs[i];
+        vbyte_encode(tf, region);
+        uint32_t prev_pos = 0;
+        for (uint32_t j = 0; j < tf; ++j) {
+            const uint32_t p = tp.positions[pi++];
+            vbyte_encode(p - prev_pos, region);
+            prev_pos = p;
+        }
+    }
+    return region;
+}
 
 }  // namespace
 
