3dbbeb65453f3622d7fa7d6b0a908d100bbfd1b1
Karthik Thallam <kthallam@andrew.cmu.edu>
2026-05-02 13:30:20 -0400

    Implement boolean evaluation and posting list cursors in Searcher

---
+++ cpp/src/searcher.cpp
@@ -0,0 +1,42 @@
+#include "atlas/searcher.hpp"
+#include <algorithm>
+
+namespace atlas {
+
+std::vector<SearchResult> Searcher::search(const std::string& query, size_t k) const {
+    std::vector<SearchResult> results;
+    auto node = parse_query(query);
+    if (!node) return results;
+
+    const auto docs = evaluate(*node);
+    for (size_t i = 0; i < std::min(docs.size(), k); ++i) {
+        const uint32_t docid = docs[i];
+        results.push_back({docid, 1.0, index_.doc_title(docid), index_.doc_url(docid)});
+    }
+    return results;
+}
+
+std::vector<uint32_t> Searcher::evaluate(const QueryNode& node) const {
+    if (node.type == NodeType::Term) {
+        return term_docs(node.term);
+    }
+    return {};
+}
+
+std::vector<uint32_t> Searcher::term_docs(const std::string& term) const {
+    std::vector<uint32_t> docs;
+    const auto* te = index_.find_term(term);
+    if (!te) return docs;
+    auto iter = index_.postings(*te);
+    while (!iter.at_end()) {
+        docs.push_back(iter.docid());
+        iter.next();
+    }
+    return docs;
+}
+
+std::vector<uint32_t> Searcher::phrase_docs(const std::vector<std::string>& terms) const {
+    return {};
+}
+
+}  // namespace atlas
